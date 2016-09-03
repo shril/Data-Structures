6fe7ea6ff052e185d1ad0fad47a70a80685c0b27
Shril Kumar <shril.iitdhn@gmail.com>
2016-09-03 17:12:54 +0530

    Added new function to LinkedList Implemenation

--- ClientLinkedList.java
+++ ClientLinkedList.java
@@ -3,15 +3,25 @@ class ClientLinkedList
     public static void main(String[] args)
     {
         LinkedList ob = new LinkedList();
-        ob.counter();
+        ob.counter(); // Checking the initial length
         
-        ob.insert("S",1);
+        // Inserting elements
+        /*ob.insert("S",1);
         ob.insert("H",2);
         ob.insert("R",3);
         ob.insert("I",4);
         ob.insert("L",5);
+        ob.printlist();*/
         
+        ob.insertEnd("S",1);
+        ob.insertEnd("H",2);
+        ob.insertEnd("R",3);
+        ob.insertEnd("I",4);
+        ob.insertEnd("L",5);
+        // Printing the elements
         ob.printlist();
+        
+        // Deleting and checking underflow
         ob.delete();
         ob.delete();
         ob.delete();
@@ -19,6 +29,21 @@ class ClientLinkedList
         ob.delete();
         ob.delete();
         ob.delete();
+        
+        ob.insertEnd("S",1);
+        ob.insertEnd("H",2);
+        ob.insertEnd("R",3);
+        ob.insertEnd("I",4);
+        ob.insertEnd("L",5);
+        
+        ob.counter();
+        
+        ob.deleteEnd();
+        ob.deleteEnd();
+        ob.deleteEnd();
+        ob.deleteEnd();
+        ob.deleteEnd();
+        ob.deleteEnd();
         
         ob.counter();
         
--- LinkedList.java
+++ LinkedList.java
@@ -10,7 +10,7 @@ class LinkedList
         start = null;
         count = 0;
     }
-    
+
     public boolean isEmpty()
     {
         return start == null;
@@ -24,34 +24,83 @@ class LinkedList
         count++;
     }
 
+    public void insertEnd(String s, int n)
+    {
+        node temp = new node(s,n);
+
+        if(start == null)
+        {
+            start = temp;
+        }
+        else
+        {
+            node trar = start;
+            while(trar.link!=null)
+            {
+                trar = trar.link;
+            }
+            trar.link = temp;
+
+        }
+        count++;
+    }
+
     public void delete()
     {
         if(start!=null)
         {
-            count--;
-            System.out.println(start.data1+">>"+start.data2);
+            System.out.println(start.data1+" : "+start.data2);
             node temp = start.link;
             start = start.link;
             temp = null; // temporary variable trar is made so that any other node
-                         // doesn't keeps pointing to first node.
+            // doesn't keeps pointing to first node.
+            count--;
         }
         else
         {
             System.out.println("Underflow");
         }
     }
-    
+
+    public void deleteEnd()
+    {
+        if(start==null)
+        {
+            System.out.println("Underflow");
+
+        }
+        else if(start.link == null)
+        {
+            System.out.println(start.data1+" : "+start.data2);
+            start = null;
+            count--;
+        }
+        else
+        {
+            node temp = start;
+            node trar = temp.link;
+            while(trar.link!=null)
+            {
+                temp = trar;
+                trar = trar.link;
+            }
+            temp.link = null;
+            System.out.println(trar.data1+" : "+trar.data2);
+            count--;
+        }
+    } 
+
     public void printlist()
     {
         node temp = start;
         while(temp!=null)
         {
-            System.out.print(temp.data1+" : "+temp.data2+" >> ");
+            System.out.print(temp.data1+" : "+temp.data2+"     ");
             temp = temp.link;
         }
         System.out.println();
     }
-    
+
     public void counter()
     {
         System.out.println("Number of items in the list are : "+this.count);
